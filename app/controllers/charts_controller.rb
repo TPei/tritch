d0e6e0f770fff454337f1759b2cc85307c17e6d6
Thomas Peikert <t.s.peikert@gmail.com>
2015-09-14 14:54:07 +0200

    ability to view line chart, rudimentary averages calculation

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -10,6 +10,7 @@ class ChartsController < ActionController::Base
 
   def twitchTotalOverTime
     @stats = StatsParser.new
+    @stats.hourly_average
     render json: @stats
   end
 end
--- app/use_cases/stats_parser.rb
+++ app/use_cases/stats_parser.rb
@@ -1,5 +1,43 @@
+require 'moving_average'
+
 class StatsParser
   def initialize
-    @stats = TwitchStat.all.pluck(:viewers)
   end
+
+  def hourly_average
+    # this works on the premise that we got exactly one data point per hour
+    # but this is not the case because we can't guarantee that the workers
+    # go 24/7 without problems.
+    # server downtime / internet disc, strange behaviours where 10 workers won't run
+    # and then suddenly 10 run at the same time
+    # we gotta use the timestamps here
+    @stats = average_viewers_per_x_hours 1
+  end
+
+  def daily_average
+    'TODO inmplement'
+  end
+
+  private
+    def data
+      data ||= TwitchStat.all.pluck(:viewers, :timestamp)
+    end
+
+    def viewers
+      data.collect(&:first)
+    end
+
+    def average_viewers_per_x_hours(hours)
+      simple_moving_average(viewers, 12*hours)
+    end
+
+    def simple_moving_average(the_stats, accuracy)
+      stats = []
+
+      (1..the_stats.length-1).each do |i|
+        stats.push(the_stats.exponential_moving_average(i, [i, accuracy].min).to_i)
+      end
+
+      return stats
+    end
 end
