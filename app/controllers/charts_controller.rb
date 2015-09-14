7b54997546d7e91f2ea1866ba7ce9616240b4a6a
Thomas Peikert <t.s.peikert@gmail.com>
2015-09-14 07:38:01 +0200

    adds chartjs and two demo routes /charts/bar /charts/line

---
+++ app/controllers/charts_controller.rb
@@ -0,0 +1,7 @@
+class ChartsController < ActionController::Base
+  def line
+  end
+
+  def bar
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'api/schedule/:hours' => 'twitch#schedule'
   get 'api/endless/:hours' => 'twitch#endless'
 
+  get 'charts/line' => 'charts#line'
+  get 'charts/bar' => 'charts#bar'
+
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 end
