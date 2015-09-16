0a58ee35b136c8a7151d5fcc171f905ee81f7b85
Thomas Peikert <t.s.peikert@gmail.com>
2015-09-16 11:49:31 +0200

    qoc improvement

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,14 +1,18 @@
 class ChartsController < ActionController::Base
 
   def total_daily
-    @stats = StatsCalculator.new
-    @stats.daily_average
+    stats.daily_average
     render json: @stats
   end
 
   def total_hourly
-    @stats = StatsCalculator.new
-    @stats.hourly_average
+    stats.hourly_average
     render json: @stats
   end
+
+  private
+
+    def stats
+      @stats ||= StatsCalculator.new
+    end
 end
